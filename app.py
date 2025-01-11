f8c8f40b9da7f9eb1ceb96c10ecce5c5a21d1749
Den Hafiz <den.m.hafiz@gmail.com>
2025-01-11 23:56:02 +0800

    Add data from Python instead of hardcoding

--- app.py
+++ app.py
@@ -3,9 +3,15 @@ from flask import render_template
 
 app=Flask(__name__, template_folder="templates/")
 
+data = {
+    "groom": "Den Hafiz",
+    "bride": "Nor Syazni",
+    "date": "14th June 2025"
+}
+
 @app.route("/")
 def index():
-    return render_template("index.html", title="Index")
+    return render_template("index.html", title="Index", data=data)
 
 if __name__ == '__main__':
     app.run(debug=True) 
