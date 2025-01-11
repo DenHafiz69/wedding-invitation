78705b08aea4c06b3832a144e860db1f796b0812
Den Hafiz <den.m.hafiz@gmail.com>
2025-01-12 00:24:44 +0800

    Add RSVP route

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from flask import Flask
-from flask import render_template
+from flask import render_template, redirect, request
 
 app=Flask(__name__, template_folder="templates/")
 
@@ -9,9 +9,19 @@ data = {
     "date": "14th June 2025"
 }
 
-@app.route("/")
+@app.route('/')
 def index():
     return render_template("index.html", title="Index", data=data)
 
+
+@app.route('/rsvp', methods=['GET', 'POST'])
+def rsvp():
+    if request.method == 'POST':
+        return render_template("success.html")
+
+    return render_template("index.html", title="Index", data=data)
+
+    
+
 if __name__ == '__main__':
-    app.run(debug=True) 
+    app.run(debug=True)
