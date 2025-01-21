fc2fd85841cbce4bfe37ac9826084afc12067af0
Den Hafiz <den.m.hafiz@gmail.com>
2025-01-21 11:21:40 +0800

    Add rsvp route

--- app.py
+++ app.py
@@ -1,6 +1,10 @@
 from flask import Flask
 from flask import render_template, redirect, request
 
+import google.auth
+from googleapiclient.discovery import build
+from googleapiclient.errors import HttpError
+
 app=Flask(__name__, template_folder="templates/")
 
 data = {
@@ -9,9 +13,24 @@ data = {
     "date": "14th June 2025"
 }
 
-@app.route('/')
+@app.route('/', methods=['POST', 'GET'])
 def index():
-    return render_template("index.html", data=data)
+    if request.method == 'GET':
+        return render_template("index.html", data=data, hidden=True)
+
+
+@app.route('/rsvp', methods=['POST'])
+def rsvp():
+
+    if request.method == 'POST':
+        # Get the values from HTML page
+        user_name = request.form['name']
+        user_phone = request.form['phone']
+        user_pax = request.form['pax']
+
+        print(f"User {user_name} has registered.")
+
+        return render_template('index.html', data=data, hidden=False)
 
 if __name__ == '__main__':
     app.run(debug=True)
